db6a64ef5926127fafdbe23c2cc011c76ca06be5
tanjinaesha <eshaislam15@gmail.com>
2022-11-24 21:43:06 +0600

    b_react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Bootstrap from './Components/Bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Bootstrap/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
